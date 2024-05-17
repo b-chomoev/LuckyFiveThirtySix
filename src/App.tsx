79c3383442a3a687f521eb7b87784478f10fc1f3
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-05-16 19:20:35 -0500

    Вывел компоненты на числа на экран с использованием useState.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 import './LotteyBall/LotteryBall'
 import LotteryBall from "./LotteyBall/LotteryBall";
+import {useState} from "react";
 
 const App = () => {
+    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
 
     const randomNumbers = () => {
 
@@ -11,13 +13,13 @@ const App = () => {
     return (
         <>
             <div className='lottery-ball'>
-                <LotteryBall number={5} />
-                <LotteryBall number={5} />
-                <LotteryBall number={5} />
-                <LotteryBall number={5} />
-                <LotteryBall number={5} />
+                <LotteryBall number={numbers[0]} />
+                <LotteryBall number={numbers[1]} />
+                <LotteryBall number={numbers[2]} />
+                <LotteryBall number={numbers[3]} />
+                <LotteryBall number={numbers[4]} />
             </div>
-            <button onClick={randomNumbers}>Click me</button>
+            <button onClick={randomNumbers}>Change Numbers</button>
         </>
     )
 }
